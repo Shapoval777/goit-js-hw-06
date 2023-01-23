eab88bf28a97fec3d1090cbe456755d119bfd463
Boris Shapoval <bshapoval2017@ukr.net>
2023-01-23 14:02:52 +0300

    Module 6

---
+++ js/task-02.js
@@ -0,0 +1,25 @@
+// Напиши скрипт, який для кожного елемента масиву ingredients:
+
+// Створить окремий елемент <li>. Обов'язково використовуй метод document.createElement().
+// Додасть назву інгредієнта як його текстовий вміст.
+// Додасть елементу клас item.
+// Після чого, вставить усі <li> за одну операцію у список ul#ingredients.
+
+const ingredients = [
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
+];
+
+const listEl = document.querySelector("#ingredients");
+console.log(listEl);
+
+for (const ingredient of ingredients) {
+  const listItem = document.createElement("li");
+  listItem.classList.add("item");
+  listItem.textContent = ingredient;
+  listEl.appendChild(listItem);
+}
